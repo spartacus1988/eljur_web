7fb02493c9274920949df79361096d4106c956f1
spartacusxxxxxx <spartacusxxxxxx@gmail.com>
2018-12-31 22:28:50 +0300

    Pagenation was added

--- eljur_web/Controllers/HomeController.cs
+++ eljur_web/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ using eljur_web.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace eljur_web
+namespace eljur_web.Controllers
 {
     public class HomeController : Controller
     {
@@ -20,11 +20,31 @@ namespace eljur_web
         }
 
         // GET: Events
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Indexxx()
         {
             //return View(await _context.Events.ToListAsync());
             return View(_context.Events);
         }
+
+        public async Task<IActionResult> Index(int page = 1)
+        {
+            int pageSize = 3;   // количество элементов на странице
+
+            IQueryable<Events> source = _context.Events;
+            var list =  source.ToList();
+            var count =  source.Count(); // CountAsync();
+            var items =  source.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+
+            PageViewModel pageViewModel = new PageViewModel(count, page, pageSize);
+            IndexViewModel viewModel = new IndexViewModel
+            {
+                PageViewModel = pageViewModel,
+                Events = items
+            };
+            return View(viewModel);
+        }
+
+
 
 
     }
---
+++ eljur_web/Models/Pagenation/IndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace eljur_web.Models
+{
+    public class IndexViewModel
+    {
+        public IEnumerable<Events> Events { get; set; }
+        public PageViewModel PageViewModel { get; set; }
+    }
+}
